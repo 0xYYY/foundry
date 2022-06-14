cd1dd55978b092daee027ce5cbb77e16c4e67e8e
0xYYY <0xYYY@protonmail.com>
2022-06-14 23:58:37 +0800

    feat(forge/doc): can display methods

--- cli/src/cmd/forge/doc.rs
+++ cli/src/cmd/forge/doc.rs
@@ -9,7 +9,11 @@ use crate::{
 };
 use askama::Template;
 use clap::{AppSettings, Parser};
-use ethers::solc::artifacts::{output_selection::OutputSelection, DevDoc, UserDoc};
+use ethers::abi::{Abi, Param, ParamType, StateMutability};
+use ethers::solc::artifacts::{
+    output_selection::OutputSelection, Contract, DevDoc, MethodDoc as SolcMethodDoc, UserDoc,
+    UserDocNotice,
+};
 use forge::executor::opts::EvmOpts;
 use foundry_common::evm::EvmArgs;
 use foundry_config::{figment::Figment, Config};
@@ -17,6 +21,7 @@ use globset::Glob;
 use regex::Regex;
 use std::collections::BTreeMap;
 use std::{
+    fmt,
     fmt::Debug,
     fs,
     path::{Path, PathBuf},
@@ -117,18 +122,187 @@ impl DocArgs {
     }
 }
 
-#[derive(Template, Debug)] // this will generate the code...
+#[derive(Template, Debug)]
 #[template(path = "doc.md")]
-struct Document {
-    file: String,
-    contracts: Vec<Contract>,
+struct FileDoc {
+    name: String,
+    contracts: Vec<ContractDoc>,
 }
 
-#[derive(Debug)] // this will generate the code...
-struct Contract {
+impl FileDoc {
+    fn new(name: String, contracts: &Vec<(String, &Contract)>) -> Self {
+        Self {
+            name,
+            contracts: contracts
+                .iter()
+                .map(|(name, contract)| ContractDoc::new(name, contract))
+                .collect(),
+        }
+    }
+}
+
+// TODO: include internal functions in the output. Would need a rewrite with AST parsing (with
+// `fmt`'s visitor implementation, since ABI only contains external functions).
+
+/// Combination of a contract's Abi, UserDoc, DevDoc
+#[derive(Debug)]
+struct ContractDoc {
     name: String,
-    userdoc: UserDoc,
-    devdoc: DevDoc,
+    title: Option<String>,
+    details: Option<String>,
+    notice: Option<String>,
+    author: Option<String>,
+    // TODO: deal with constructor, receiver function and fallback
+    methods: BTreeMap<String, Vec<MethodDoc>>,
+    events: BTreeMap<String, Vec<EventDoc>>,
+    errors: BTreeMap<String, Vec<ErrorDoc>>,
+}
+
+impl ContractDoc {
+    fn new(name: &String, contract: &Contract) -> Self {
+        let abi = &contract.abi.as_ref().unwrap().abi;
+        let devdoc = &contract.devdoc;
+        let userdoc = &contract.userdoc;
+        Self {
+            name: name.to_string(),
+            title: devdoc.title.clone(),
+            details: devdoc.details.clone(),
+            notice: userdoc.notice.clone(),
+            author: devdoc.author.clone(),
+            methods: Self::parse_methods(abi, &devdoc, &userdoc),
+            events: Self::parse_events(abi, &devdoc, &userdoc),
+            errors: Self::parse_errors(abi, &devdoc, &userdoc),
+        }
+    }
+
+    fn parse_methods(
+        abi: &Abi,
+        dev_doc: &DevDoc,
+        user_doc: &UserDoc,
+    ) -> BTreeMap<String, Vec<MethodDoc>> {
+        let mut methods: BTreeMap<String, Vec<MethodDoc>> = BTreeMap::new();
+        for function in abi.functions() {
+            let signature = function.signature();
+            let signature = signature.split(':').next().unwrap();
+            let function_dev_doc =
+                dev_doc.methods.get(signature).cloned().unwrap_or(SolcMethodDoc::default());
+            let function_user_doc = user_doc.methods.get(signature);
+            let params = Self::parse_params(&function.inputs, &function_dev_doc.params);
+            let returns = Self::parse_params(&function.outputs, &function_dev_doc.returns);
+            methods.entry(function.name.clone()).or_insert(Vec::new()).push(MethodDoc {
+                name: function.name.clone(),
+                details: function_dev_doc.details.clone(),
+                notice: match function_user_doc {
+                    Some(UserDocNotice::Constructor(x)) => Some(x.clone()),
+                    Some(UserDocNotice::Notice { notice: x }) => Some(x.clone()),
+                    None => None,
+                },
+                state_mutability: function.state_mutability,
+                params,
+                returns,
+            })
+        }
+        methods
+    }
+
+    fn parse_events(
+        abi: &Abi,
+        devdoc: &DevDoc,
+        userdoc: &UserDoc,
+    ) -> BTreeMap<String, Vec<EventDoc>> {
+        let events: BTreeMap<String, Vec<EventDoc>> = BTreeMap::new();
+        events
+    }
+
+    fn parse_errors(
+        abi: &Abi,
+        devdoc: &DevDoc,
+        userdoc: &UserDoc,
+    ) -> BTreeMap<String, Vec<ErrorDoc>> {
+        let errors: BTreeMap<String, Vec<ErrorDoc>> = BTreeMap::new();
+        errors
+    }
+
+    fn parse_params(params: &Vec<Param>, param_docs: &BTreeMap<String, String>) -> Vec<ParamDoc> {
+        params
+            .iter()
+            .map(|p| ParamDoc {
+                name: if p.name.is_empty() { String::from("-") } else { p.name.clone() },
+                kind: p.kind.clone(),
+                internal_type: p.internal_type.clone(),
+                indexed: None,
+                doc: param_docs.get(&p.name.clone()).cloned().unwrap_or(String::from("-")),
+            })
+            .collect()
+    }
+}
+
+#[derive(Debug)]
+struct MethodDoc {
+    name: String,
+    details: Option<String>,
+    notice: Option<String>,
+    state_mutability: StateMutability,
+    params: Vec<ParamDoc>,
+    returns: Vec<ParamDoc>,
+}
+
+impl fmt::Display for MethodDoc {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let state_mutability = match self.state_mutability {
+            StateMutability::Pure => " pure".to_string(),
+            StateMutability::View => " view".to_string(),
+            StateMutability::NonPayable => " nonpayable".to_string(),
+            StateMutability::Payable => " payable".to_string(),
+        };
+        let params =
+            self.params.iter().map(|x| format!("{}", x)).collect::<Vec<String>>().join(", ");
+        let returns = if self.returns.len() > 0 {
+            format!(
+                " returns ({})",
+                self.returns.iter().map(|x| format!("{}", x)).collect::<Vec<String>>().join(", ")
+            )
+        } else {
+            String::new()
+        };
+        write!(f, "{}({}) external{}{}", self.name, params, state_mutability, returns)
+    }
+}
+
+#[derive(Debug)]
+struct EventDoc {
+    name: String,
+    details: Option<String>,
+    notice: Option<String>,
+    params: Vec<ParamDoc>,
+}
+
+#[derive(Debug)]
+struct ErrorDoc {
+    name: String,
+    details: Option<String>,
+    notice: Option<String>,
+    params: Vec<ParamDoc>,
+}
+
+#[derive(Debug)]
+struct ParamDoc {
+    name: String,
+    kind: ParamType,
+    internal_type: Option<String>,
+    /// for Event params
+    indexed: Option<bool>,
+    doc: String,
+}
+
+impl fmt::Display for ParamDoc {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        if self.name.is_empty() {
+            write!(f, "{}", self.kind)
+        } else {
+            write!(f, "{} {}", self.kind, self.name)
+        }
+    }
 }
 
 impl Cmd for DocArgs {
@@ -143,7 +317,10 @@ impl Cmd for DocArgs {
         // TODO: better way to set this up?
         project.solc_config.settings.output_selection = OutputSelection(BTreeMap::from([(
             "*".to_string(),
-            BTreeMap::from([("*".to_string(), vec!["devdoc".to_string(), "userdoc".to_string()])]),
+            BTreeMap::from([(
+                "*".to_string(),
+                vec!["abi".to_string(), "devdoc".to_string(), "userdoc".to_string()],
+            )]),
         )]));
         let compiler = ProjectCompiler::default();
         let output = if self.opts.silent {
@@ -154,29 +331,71 @@ impl Cmd for DocArgs {
 
         let src_dir = config.src.to_str().unwrap();
         let src_dir_glob = Glob::new(format!("{}/*", src_dir).as_str())?;
-        let documents: Vec<Document> = output
-            .output()
-            .contracts
-            .0
+        // let documents: Vec<Document> = output
+        // output
+        //     .output()
+        //     .contracts
+        //     .0
+        //     .iter()
+        //     .filter(|(file, _)| src_dir_glob.compile_matcher().is_match(file))
+        //     // .map(|(file, contracts)| Document {
+        //     //     file: file
+        //     //         .to_string()
+        //     //         .strip_prefix(format!("{}/", src_dir).as_str())
+        //     //         .unwrap()
+        //     //         .strip_suffix(".sol")
+        //     //         .unwrap()
+        //     //         .to_string(),
+        //     //     contracts: contracts
+        //     //         .iter()
+        //     //         .map(|(name, contract)| Contract {
+        //     //             name: name.to_string(),
+        //     //             userdoc: contract[0].clone().contract.userdoc,
+        //     //             devdoc: contract[0].clone().contract.devdoc,
+        //     //             abi: contract[0].clone().contract.abi.unwrap().abi,
+        //     //         })
+        //     //         .collect(),
+        //     // })
+        //     // .collect();
+        //     .for_each(|(file, contracts)| {
+        //         println!(
+        //             "{}",
+        //             file.to_string()
+        //                 .strip_prefix(format!("{}/", src_dir).as_str())
+        //                 .unwrap()
+        //                 .strip_suffix(".sol")
+        //                 .unwrap()
+        //                 .to_string()
+        //         );
+        //         for (name, contract) in contracts {
+        //             println!("{}", name);
+        //             println!("{:?}", contract[0].clone().contract.abi.unwrap().abi);
+        //             println!("{:?}", contract[0].clone().contract.devdoc);
+        //             println!("{:?}", contract[0].clone().contract.userdoc);
+        //         }
+        //     });
+        let output = output.output();
+        let mut grouped_contracts: BTreeMap<String, Vec<(String, &Contract)>> = BTreeMap::new();
+        for (file, name, contract) in output.contracts_with_files_iter() {
+            if !src_dir_glob.compile_matcher().is_match(file) {
+                continue;
+            }
+            grouped_contracts
+                .entry(
+                    file.to_string()
+                        .strip_prefix(format!("{}/", src_dir).as_str())
+                        .unwrap()
+                        .strip_suffix(".sol")
+                        .unwrap()
+                        .to_string()
+                        .into(),
+                )
+                .or_insert(Vec::new())
+                .push((name.into(), &contract));
+        }
+        let documents: Vec<FileDoc> = grouped_contracts
             .iter()
-            .filter(|(file, _)| src_dir_glob.compile_matcher().is_match(file))
-            .map(|(file, contracts)| Document {
-                file: file
-                    .to_string()
-                    .strip_prefix(format!("{}/", src_dir).as_str())
-                    .unwrap()
-                    .strip_suffix(".sol")
-                    .unwrap()
-                    .to_string(),
-                contracts: contracts
-                    .iter()
-                    .map(|(name, contract)| Contract {
-                        name: name.to_string(),
-                        userdoc: contract[0].clone().contract.userdoc,
-                        devdoc: contract[0].clone().contract.devdoc,
-                    })
-                    .collect(),
-            })
+            .map(|(file, contracts)| FileDoc::new(file.to_string(), contracts))
             .collect();
 
         let mut doc_dir = PathBuf::new();
@@ -187,14 +406,11 @@ impl Cmd for DocArgs {
         if !doc_dir.exists() {
             fs::create_dir(doc_dir)?;
         }
-        println!("doc_dir: {}", doc_dir.to_str().unwrap());
         for document in documents {
             let document_string = document.render()?;
             let mut document_path =
                 Path::new(&format!("{}", doc_dir.to_str().unwrap())).to_path_buf();
-            println!("{}", document_path.to_str().unwrap());
-            document_path.push(format!("{}.md", document.file));
-            println!("{}", document_path.to_str().unwrap());
+            document_path.push(format!("{}.md", document.name));
             if !document_path.parent().unwrap().exists() {
                 fs::create_dir(document_path.parent().unwrap())?;
             }
--- cli/templates/doc.md
+++ cli/templates/doc.md
@@ -1,19 +1,82 @@
 {% for contract in contracts %}
 # {{ contract.name }}.sol
 
-## User Doc
-### Methods
-{% for (func, doc) in contract.userdoc.methods %}
-```
-{{ func }}
-```
-{% endfor %}
+    {% match contract.title %}
+        {%- when Some with (title) %}
+{{ title }}
+        {%- when None %}
+    {%- endmatch -%}
 
-### Events
-### Errors
+    {% match contract.author %}
+        {%- when Some with (author) %}
+**Author: {{ author -}}**
+        {%- when None %}
+    {%- endmatch -%}
 
-## Dev Doc
-### Methods
+    {% match contract.details %}
+        {%- when Some with (details) %}
+{{ details -}}
+        {%- when None %}
+    {%- endmatch %}
+
+    {% match contract.notice %}
+        {%- when Some with (notice) %}
+*{{ notice -}}*
+        {%- when None %}
+    {%- endmatch %}
+
+{%- if contract.methods.len() > 0 %}
+## Methods
+        {%- for (name, methods) in contract.methods %}
+### {{ name }}
+        {%- for method in methods %}
+```solidity
+{{ method }}
+```
+
+            {% match contract.details %}
+                {%- when Some with (details) %}
+{{ details -}}
+                {%- when None %}
+            {%- endmatch %}
+
+            {% match contract.notice %}
+                {%- when Some with (notice) %}
+*{{ notice -}}*
+                {%- when None %}
+            {%- endmatch %}
+
+{%- if method.params.len() > 0 %}
+#### Parameters
+
+| Name | Type | Description |
+|---|---|---|
+{% for param in method.params -%}
+| {{ param.name }} | {{ param.kind }} | {{ param.doc }} |
+{% endfor -%}
+{%- endif %}
+
+{%- if method.returns.len() > 0 %}
+#### Return Values
+
+| Name | Type | Description |
+|---|---|---|
+{% for param in method.returns -%}
+| {{ param.name }} | {{ param.kind }} | {{ param.doc }} |
+{% endfor -%}
+{%- endif %}
+
+{%- endfor %}
+
+{%- endfor %}
+{%- endif %}
+
+{%- if contract.events.len() > 0 %}
 ### Events
+{%- endif %}
+
+{%- if contract.errors.len() > 0 %}
 ### Errors
+{%- endif %}
+
 {% endfor %}
